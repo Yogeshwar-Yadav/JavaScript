8e6f64ae5c502882524f164eabea58fb47774019
Yogeshwar Yadav <121799062+Yogeshwar-Yadav@users.noreply.github.com>
2024-10-02 15:19:13 +0000

    Conversions

---
+++ JSBasics/Conversion.js
@@ -0,0 +1,38 @@
+let score = 33
+
+console.log(score)
+console.log(typeof score)
+
+
+
+let newscore = "33"
+
+console.log(newscore)
+console.log(typeof newscore)
+
+
+
+let newscorevalue =  "85"
+let valueINumnber = Number(newscorevalue)
+
+console.log(valueINumnber)
+console.log(typeof valueINumnber)
+
+
+let value = "25acss"
+let stringvalueINumnber = Number(value)
+
+console.log(stringvalueINumnber)
+console.log(typeof stringvalueINumnber)
+
+let isLoggedIN = 0
+let booleanIsLoggedIn = Boolean(isLoggedIN)
+
+console.log(booleanIsLoggedIn)
+console.log(typeof booleanIsLoggedIn)
+
+let val = 12
+let stringValue = String(val)
+
+console.log(stringValue)
+console.log(typeof stringValue)
