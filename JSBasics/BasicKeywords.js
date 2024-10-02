ce435a20da0c36a5f7d2cc49192b0b3b3c77f80e
Yogeshwar Yadav <121799062+Yogeshwar-Yadav@users.noreply.github.com>
2024-10-02 14:26:56 +0000

    DataTypes

---
+++ JSBasics/BasicKeywords.js
@@ -0,0 +1,8 @@
+const DOB = 28082001
+let name ="Yogeshwar Yadav" //only preferable for initializing a variable
+var email = "yogeshwaryadav42@gmail.com" //Not preferable
+accountType ="Public"//not preferable
+
+console.log(DOB)
+
+console.table([DOB,name,email,accountType])
---
+++ JSBasics/dataTypes.js
@@ -0,0 +1,8 @@
+let name ="Yogeshwar Yadav"  //string
+let age = 23  //number
+let isloggedin = false  //boolean
+
+console.log(typeof name)
+console.log(typeof age)
+console.log(typeof null)
+console.log(typeof undefined)
