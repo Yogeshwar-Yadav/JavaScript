2575984348eb4859f4f45919ef61a6b3a257a22a
Yogeshwar Yadav <121799062+Yogeshwar-Yadav@users.noreply.github.com>
2024-10-04 16:35:50 +0000

    DataTypeSummary

---
+++ JSBasics/DataTypeSummary.js
@@ -0,0 +1,28 @@
+const score = 23;
+const newscore = 23.3
+const isLoggedIn = true
+const temp = null
+let useremail
+const id = Symbol(123)
+const userID = Symbol(123)
+
+console.log(id === userID)
+const bigNumber = 34234323n
+
+
+//Non Primitive Data Types
+//Array
+const arr = ["Yogeshwar","Yadav","Saayan","Das"]
+//object
+let myobj = {
+    name: "Yogeshwar Yadav",
+    age: 23
+}
+
+//Functions
+let myFunction = function()
+{
+    console.log("Hello Mr Yogeshwar Yadav");
+}
+
+myFunction();
