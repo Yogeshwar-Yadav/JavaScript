09ef9ba32c6738b2023f2178b26f0cfe6d58e5a7
Yogeshwar Yadav <121799062+Yogeshwar-Yadav@users.noreply.github.com>
2024-10-04 12:38:40 +0000

    Comparisons

---
+++ JSBasics/Comparisons.js
@@ -0,0 +1,15 @@
+console.log(2>1) //true
+console.log(2<1) //false
+console.log(2>=1) //true
+
+console.log("2" >1) //t
+console.log("2"==2) //t
+console.log("02" ==2) //t
+console.log("02" >= 2) //t
+
+console.log("2"===2) //f
+
+console.log(null == 0) //f
+console.log(undefined == 0) //f
+console.log(null===0)//f
+console.log(undefined === 0) //f
